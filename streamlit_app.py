cc50d0baa4774449a09c2a798a2139efb76182c6
250706 <mochzeaalfatih@gmail.com>
2025-07-18 23:15:21 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,16 +6,7 @@ import altair as alt
 st.set_page_config(page_title="Kalkulator Konversi Satuan Fisika", layout="centered")
 st.title("🔬 KALKULATOR KONVERSI SATUAN FISIKA")
 st.markdown("Konversi berbagai satuan fisika lengkap dengan penjelasan dan grafik hasil.")
-st.markdown(
-    .stApp {
-        background: linear-gradient(rgba(0, 0, 0, 0.85), rgba(0, 0, 0, 0.85)),
-                    url('https://cdn.bhdw.net/im/chemistry-and-physics-symbols-on-black-board-wallpaper-108136_w635.webp');
-        background-size: cover;
-        background-position: center;
-        background-repeat: no-repeat;
-    }
- unsafe_allow_html=True
-)
+
 # Data konversi dan faktor satuan
 konversi_data = {
     "🔥 Suhu": {
