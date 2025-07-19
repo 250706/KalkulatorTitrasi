b0041c4cb88357a235b31883be16ae66c4038657
250706 <mochzeaalfatih@gmail.com>
2025-07-19 15:00:02 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -272,5 +272,6 @@ melakukan konversi satuan fisika secara akurat dan cepat.
 - *Physics for Scientists and Engineers*  Serway & Jewett
 - *CRC Handbook of Chemistry and Physics*
 - *Thermodynamics*  Yunus Cengel
-""")  
+"""
+)  
 
