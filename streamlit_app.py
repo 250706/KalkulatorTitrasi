17f818c6f62dfe0847d4e5420d1a523adfc40b68
250706 <mochzeaalfatih@gmail.com>
2025-07-19 14:50:18 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -269,8 +269,8 @@ melakukan konversi satuan fisika secara akurat dan cepat.
 ### 📚 Sumber Referensi:
 - SI Units: https://www.bipm.org
 - NIST  (National Institute of Standards and Technology)
-- Physics for Scientists and Engineers – Serway & Jewett
+- Physics for Scientists and Engineers  Serway & Jewett
 - CRC Handbook of Chemistry and Physics
-- Thermodynamics – Yunus Cengel
+- Thermodynamics  Yunus Cengel
 """)
 
