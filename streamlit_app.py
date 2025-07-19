b004065af542dd0c3e02463168df4d716b7f32b8
250706 <mochzeaalfatih@gmail.com>
2025-07-19 21:59:46 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,27 +7,13 @@ import altair as alt
 def set_background_from_url(image_url):
     st.markdown(f"""
         <style>
-        .stApp {{
-            background-image: url('https://i.pinimg.com/originals/4e/ac/35/4eac359bce1e49679ad98f98db7428d4.png');
-            background-size: cover;
-            background-position: center;
-            background-repeat: no-repeat;
-            background-attachment: fixed;
-        }}
-        .stApp::before {{
-            content: "";
-            position: absolute;
-            top: 0;
-            left: 0;
-            right: 0;
-            bottom: 0;
-            background-color: rgba(0, 0, 0, 0.85);
-            z-index: -1;
-        }}
-        </style>
-    """, unsafe_allow_html=True)
-
-set_background_from_url("https://i.pinimg.com/originals/4e/ac/35/4eac359bce1e49679ad98f98db7428d4.png")
+        .stApp {
+        background: linear-gradient(rgba(0, 0, 0, 0.85), rgba(0, 0, 0, 0.85)),
+                    url('https://i.pinimg.com/originals/4e/ac/35/4eac359bce1e49679ad98f98db7428d4.png');
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+    }
 
 # ---------------------- EFEK INTERAKTIF ----------------------
 st.markdown("""
